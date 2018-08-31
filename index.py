efc6dc7563c61bfd4f163a20017859f07039b5f4
Robert <rlinden@g.hmc.edu>
2018-08-31 00:16:24 -0400

    SOme great stuff coming along

--- index.py
+++ index.py
@@ -1,6 +1,16 @@
-from src.tktable import Example
+from src.tkspreadsheet import Example
 import tkinter as tk
+import os
 
-root = tk.Tk()
-Example(root).pack(side="top", fill="both", expand=True)
-root.mainloop()
+def main():
+	root = tk.Tk()
+
+	program_paths = {}
+	program_paths['index'] = os.path.dirname(os.path.realpath(__file__))
+	program_paths['icons'] = os.path.join(program_paths['index'], 'resources', 'icons')
+
+	Example(root, program_paths = program_paths).pack(side="top", fill="both", expand=True)
+	root.mainloop()
+
+if __name__ == '__main__':
+	main()
